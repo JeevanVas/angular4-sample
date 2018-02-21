e451b062ae592e1fe12bf257b86958f4f2c8f5fa
JeevanVas <jeevanmvas@gmail.com>
2018-02-21 10:57:09 +0530

    Routing in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TutorialsComponent } from './tutorials/tutorials.component';
@@ -10,6 +11,7 @@ import { UserDataTdfComponent } from './user-data-tdf/user-data-tdf.component';
 import { UserDataMdfComponent } from './user-data-mdf/user-data-mdf.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
 
 
 @NgModule({
@@ -19,13 +21,18 @@ import { EmployeeDetailComponent } from './employee-detail/employee-detail.compo
     UserDataTdfComponent,
     UserDataMdfComponent,
     EmployeeListComponent,
-    EmployeeDetailComponent
+    EmployeeDetailComponent,
+    DepartmentListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path:'departments', component:DepartmentListComponent},
+      {path:'employees', component:EmployeeListComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
