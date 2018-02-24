6f3d404c0cf53cc0d6e569c55cf3d2e926c8b873
JeevanVas <jeevanmvas@gmail.com>
2018-02-24 16:47:56 +0530

    Routing parameters

--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-department-list',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  departments = [
+    {"id": 1, "name": "Angular"},
+    {"id": 2, "name": "ReactJs"},
+    {"id": 3, "name": "Dojo"},
+    {"id": 4, "name": "Javascript"},
+    {"id": 5, "name": "Bootstrap"},
+  ];
+
+  onSelect(department){
+    this.router.navigate(['/departments', department.id]);
   }
 
 }
