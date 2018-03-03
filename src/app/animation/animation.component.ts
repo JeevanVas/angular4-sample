09b056fbded24d7bb8b65060144b72246edd2461
JeevanVas <jeevanmvas@gmail.com>
2018-03-03 23:28:53 +0530

    Multistep Animation with Keyframes

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -54,12 +54,19 @@ import { Component, OnInit,
     // transition('void => *', [
      transition(':enter', [
       style({transform: 'translateX(-100%)'}),
-      animate('1s')
+      animate(1000, keyframes([
+        style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+        style({opacity: 1, transform: 'translateX(15px)', offset: 0.3}),
+        style({opacity: 1, transform: 'translateX(0)', offset: 1.0})
+      ]))
      ]),
      //transition('* => void', [
      transition(':leave', [
-       animate('1s', style({transform: 'translateX(100%)'}))
-       
+      animate(1000, keyframes([
+        style({opacity: 1, transform: 'translateX(0)', offset: 0}),
+        style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
+        style({opacity: 0, transform: 'translateX(100%)', offset: 1.0})
+      ]))
      ])
 
    ])
