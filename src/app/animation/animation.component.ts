9b50d2d20d21d614c514ca0bb9fdf403d7992a60
JeevanVas <jeevanmvas@gmail.com>
2018-03-03 23:15:49 +0530

    Transition Styling, animation timing and callbacks

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -20,8 +20,9 @@ import { Component, OnInit,
       state('on', style({
         backgroundColor: 'white'
       })),
-      transition('off => on', [animate('2s')]),
-      transition('on => off', [animate('2s')])
+     // transition('off <=> on', [animate('2s', style({transform: 'rotate(90deg)'}))]),
+      transition('off => on', [animate('2000ms 2s ease-in', style({transform: 'rotate(90deg)'}))]),
+      transition('on => off', [animate('2s ease-out', style({transform: 'rotate(-90deg)'}))]),
     ])
   ]
 })
@@ -35,5 +36,16 @@ export class AnimationComponent implements OnInit {
   toggleLights(){
     this.roomState = (this.roomState === "off") ? "on" : "off";
   }
-
+  animationStarted(event: any){
+    console.log('Animation started');
+    console.log(event.fromState);
+    console.log(event.toState);
+    console.log(event.totalTime);
+  }
+  animationDone(event: any){
+    console.log(event.fromState);
+    console.log(event.toState);
+    console.log(event.totalTime);
+    console.log('Animation Done');
+  }
 }
